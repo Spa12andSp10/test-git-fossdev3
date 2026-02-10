93dbac9173867da8c50c6b052a5b0660972312dd
Spa12andSp10 <sergey.pleskunov.2006@gmail.com>
2026-02-10 14:38:10 +0800

    Added list division check

--- script.py
+++ script.py
@@ -4,6 +4,10 @@ def sum(a, b):
 def devide(a, b):
     if b == 0:
         raise ValueError("Denominator could not be zero!!!")
+    if isinstance(a, str) or isinstance(b, str):
+        raise ValueError("Error!")
+    if isinstance(a, list) or isinstance(b, list):
+        raise ValueError("Error_number_2")
     return a / b
 
 def mul(a, b):
