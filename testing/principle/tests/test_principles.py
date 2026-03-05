de0b00d58077344bf7509575fecaa7b624a18951
Spa12andSp10 <sergey.pleskunov.2006@gmail.com>
2026-03-05 12:56:42 +0800

    Show test-may-not-show errors

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -1,10 +1,20 @@
 import sys
 sys.path.append("../scr")
 
-from math_demo import add
+from math_demo import add, add_with_bug
 
 def test_addition():
-    assert 2 + 2 == 4
+    assert add(2, 2) == 4
+    assert add(0, 0) == 0
+    assert add(7, 6) == 13
+    print("Test Addition past")
+
+def test_addition_with_bug():
+    assert add_with_bug(2, 2) == 4
+    assert add_with_bug(0, 0) == 0
+    #assert add_with_bug(7, 6) == 13
+    print("Test bugged Addition past")
 
 if __name__ == '__main__':
     test_addition()
+    test_addition_with_bug()
